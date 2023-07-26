83f1eff337417a7b5f392f1a4449f30a6b62418b
juhyeseong <130978861+juhyeseong@users.noreply.github.com>
2023-07-26 16:52:08 +0900

    자바스크립트의 event 발생 처리 함수와 arr의 sort 실습 및 퀴즈

---
+++ day03/weather_20230726.js
@@ -0,0 +1,67 @@
+let arr = [
+    {
+        "date": "29일(토) 오전",
+        "weather": "구름많음",
+        "rainPercent": "20%"
+    },
+    {
+        "date": "29일(토) 오후",
+        "weather": "구름많음",
+        "rainPercent": "10%"
+    },
+    {
+        "date": "30일(일) 오전",
+        "weather": "맑음",
+        "rainPercent": "10%"
+    },
+    {
+        "date": "30일(일) 오후",
+        "weather": "맑음",
+        "rainPercent": "10%"
+    },
+    {
+        "date": "31일(월) 오전",
+        "weather": "구름많음",
+        "rainPercent": "20%"
+    },
+    {
+        "date": "31일(월) 오후",
+        "weather": "구름많음",
+        "rainPercent": "10%"
+    },
+    {
+        "date": "01일(화) 오전",
+        "weather": "구름많음",
+        "rainPercent": "20%"
+    },
+    {
+        "date": "01일(화) 오후",
+        "weather": "구름많음",
+        "rainPercent": "20%"
+    },
+    {
+        "date": "02일(수) 오전",
+        "weather": "구름많음",
+        "rainPercent": "30%"
+    },
+    {
+        "date": "02일(수) 오후",
+        "weather": "구름많음",
+        "rainPercent": "40%"
+    },
+    {
+        "date": "03일(목)",
+        "weather": "흐림",
+        "rainPercent": "40%"
+    },
+    {
+        "date": "04일(금)",
+        "weather": "흐림",
+        "rainPercent": "40%"
+    },
+    {
+        "date": "05일(토)",
+        "weather": "흐림",
+        "rainPercent": "40%"
+    }
+]
